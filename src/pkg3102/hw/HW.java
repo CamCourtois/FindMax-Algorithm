d48592d9dd55515c03ac4fb4d8b953afe843a4e5
CamCourtois <92766289+CamCourtois@users.noreply.github.com>
2022-03-26 13:24:05 -0500

    This is a FindMax algorithm for a particular array w/ an increasing to decreasing property

---
+++ src/pkg3102/hw/HW.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3102.hw;
+
+/**
+ *
+ * @author cameroncourtois
+ */
+public class HW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+         int array[] = {1,2};
+         int max = array.length-1;
+         int min = 0;
+         int result = findMax(array,min,max);
+         System.out.println("The max element is "+ result);   
+            
+    }
+
+    public static int findMax(int array[], int min, int max)
+    {
+
+        while(min < max)
+        {
+            
+            int mid = (max + min)/2;
+            
+            if(min > max){
+                return array[min];
+            }
+            
+            if(max == min + 1 && array[min] >= array[max])
+                return array[min];
+            
+            if(array[mid] > array[mid - 1] && array[mid+1] < array[mid]){
+                return array[mid];
+            }
+            else if(array[mid] < array[mid+1]){
+                min = mid;
+            }
+            else
+                max = mid-1;
+           
+            
+        }
+        return -1;
+    }
+    
+}
